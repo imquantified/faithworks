8b008d51df2c976214205770b53fea1afc2eb091
Seth Quant <seth@brickwire.com>
2015-04-08 18:50:21 +0900

    Added edit and delete options

--- faithworks.js
+++ faithworks.js
@@ -1,9 +1,13 @@
-// simple-todos.js
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
+
 	Template.registerHelper("prettyDate", function(timestamp) {
 		return moment(new Date(timestamp)).fromNow();
+	});
+
+	Template.registerHelper("isEditable", function() {
+		return Session.equals("editTask", true);
 	});
 
 	Template.body.helpers({
@@ -52,6 +56,37 @@ if (Meteor.isClient) {
 
 			// Prevent default form submit
 			return false;
+		},
+
+		"click .delete": function (event) {
+			Tasks.remove(this._id);
+		},
+
+		"click .edit-task": function (event) {
+			var current = Session.get("editTask");
+
+			if (current == true) { 
+				Session.set("editTask", false); 
+			}
+			else { 
+				Session.set("editTask", true); 
+			}
+		},
+		"submit .edit-field": function (event) {
+			var text = event.target.text.value;
+			var userId = Meteor.userId();
+			var task = Tasks.find(this._id);
+
+			//if (userId == task.owner) {
+				Tasks.update(this._id, {$set: {text: text}});
+				// Clear form
+				event.target.text.value = "";
+			//}
+			
+			// Prevent default form submit
+			return false;
+
+			Session.set("editTask", false);
 		}
 	});
 
